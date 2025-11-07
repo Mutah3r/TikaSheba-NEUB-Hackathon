9b1195af8680af8566ba8af6854d7bef517d9018
Mutah3r <evil.programmer.stephen@gmail.com>
2025-11-08 05:08:04 +0600

    integrate scheduling

--- frontend/src/services/appointmentService.js
+++ frontend/src/services/appointmentService.js
@@ -4,4 +4,8 @@ export function getTodaysScheduledByCentre(centre_id) {
   return api.get(`/appointment/centre/${centre_id}/status/scheduled/date/today`);
 }
 
-export default { getTodaysScheduledByCentre };
+export function getCentreCapacityNext30(centre_id) {
+  return api.get(`/capacity/available/centre/${centre_id}/next30`);
+}
+
+export default { getTodaysScheduledByCentre, getCentreCapacityNext30 };
